808719bb9790ef01baaae1ca258ad35d3b8fbe0e
Diego Alonso <alonso.morey.arq@gmail.com>
2021-09-17 21:42:52 -0700

    correcting mistakes

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
+    if a_dictionary is None or a_dictionary == {}:
         return None
-    return max(a_dictionary)
+
+    order = sorted(a_dictionary.values())
+    best = order[-1]
+    for k, v in a_dictionary.items():
+        if v == best:
+            return k
