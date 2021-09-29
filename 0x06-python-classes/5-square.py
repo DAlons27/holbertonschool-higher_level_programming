4a0c825ffbef971dcb0f64d8e4b40508dcc04332
Diego Alonso <alonso.morey.arq@gmail.com>
2021-09-28 22:11:55 -0700

    correcting mistakes

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 """Class Square"""
 
+
 class Square:
     """Represents a square"""
 
     def __init__(self, size=0):
-        """Initializes the data."""
+        """Initializes the data"""
         self.__size = size
 
-    @property
     def size(self):
-        """Retrieves the size."""
+        """Retrieves the size"""
         return self.__size
 
-    @size.setter
     def size(self, value):
-        """Sets the size to a value."""
+        """Sets the size to a value"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -27,7 +26,7 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        """Prints to stdout the square with the character #."""
+        """Prints to stdout the square with the character"""
         if self.__size == 0:
             print()
         else:
