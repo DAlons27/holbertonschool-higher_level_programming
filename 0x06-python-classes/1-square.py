1d093bba602a33b993ffe80f9aca08dff75889f2
Diego Alonso <alonso.morey.arq@gmail.com>
2021-09-28 21:37:26 -0700

    Removing comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,11 +3,7 @@
 
 
 class Square:
-    """Represents a square.
-    Private instance attribute: size.
-    Instantiation with size (no type/value verification).
-    """
-
+    """Represents a square"""
     def __init__(self, size):
         """Initializes the data."""
         self.__size = size
