fc4a20e6d471177dbedc290f3d8ce8b067d231a6
Diego Alonso <alonso.morey.arq@gmail.com>
2021-09-28 22:14:28 -0700

    correcting mistakes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,32 +4,28 @@ class Square:
 
 
     def __init__(self, size=0, position=(0, 0)):
-        """Initializes the data."""
+        """Initializes the data"""
         self.size = size
         self.position = position
 
-    @property
     def size(self):
-        """Retrieves the size."""
+        """Retrieves the size"""
         return self.__size
 
-    @size.setter
     def size(self, value):
-        """Sets the size to a value."""
+        """Sets the size to a value"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    @property
     def position(self):
-        """Retrieves the position."""
+        """Retrieves the position"""
         return self.__position
 
-    @position.setter
     def position(self, value):
-        """Sets the position to a value."""
+        """Sets the position to a value"""
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if not isinstance(value[0], int) or not isinstance(value[1], int):
