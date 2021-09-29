fa83951c3600b15abd397ef55ae642ef8a751bf4
Diego Alonso <alonso.morey.arq@gmail.com>
2021-09-28 22:00:38 -0700

    Adding comment task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
 class Square:
-    """Represents a square.
-    Private instance attribute: size:
-        - property def size(self)
-        - property setter def size(self, value)
-    Private instance attribute: position:
-        - property def position(self)
-        - property setter def position(self, value)
-    Instantiation with optional size and optional position.
-    Public instance method: def area(self).
-    Public instance method: def my_print(self).
-    """
+    """Represents a square"""
+
 
     def __init__(self, size=0, position=(0, 0)):
         """Initializes the data."""
@@ -49,12 +40,12 @@ class Square:
 
     def area(self):
         """Returns the current square area."""
+
         return self.__size ** 2
 
     def my_print(self):
-        """Prints to stdout the square with the character #,
-        at the position given by the position attribute.
-        """
+        """Prints to stdout the square with the character"""
+        
         if self.__size == 0:
             print()
             return
