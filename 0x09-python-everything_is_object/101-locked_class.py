c762f276add7133b4b364b3e1d6a1283b0d48c80
Diego Alonso <alonso.morey.arq@gmail.com>
2021-10-05 21:59:11 -0700

    Fix Task

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,8 +3,6 @@
 
 
 class LockedClass:
+ """Class of LockedClas"""
+     __slots__ = ['first_name']
 
-    __slots__ = ['first_name']
-
-    def __init__(self, first_name=''):
-        self.first_name = first_name
