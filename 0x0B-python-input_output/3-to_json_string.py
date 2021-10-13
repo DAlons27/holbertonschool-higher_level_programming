b5c0dc9d27b14314d258368cefe23cd6c6014f59
DAlons <alonso.morey.arq@gmail.com>
2021-10-12 21:45:17 -0500

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Returns the JSON representation of an object"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """Returns th JSON representation of my_obj.
+    Args:
+        - my_obj: string to represent
+    Returns: JSON representation
+    """
+
+    return json.dumps(my_obj)
