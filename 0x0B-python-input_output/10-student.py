1f297769e655f64c2e83882ca7b684e60c404ad7
DAlons <alonso.morey.arq@gmail.com>
2021-10-12 22:59:02 -0500

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Create class student"""
+
+
+class Student():
+    """Student Class"""
+
+    def __init__(self, first_name, last_name, age):
+        """initalitation method"""
+        
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """that retrieves a dictionary representation of a Student instance"""
+        return vars(self)
+
+        def to_json(self, attrs=None):
+        """that retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+                return self.__dict__
+                my_dict = {}
+            for attr in attrs:
+                if attr in self.__dict__.keys():
+                    my_dict[attr] = self.__dict__[attr]
+            return my_dict
