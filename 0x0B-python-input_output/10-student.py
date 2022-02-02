5cf0686ce2ea77251c8e3eed6bb8774cdccdb904
DAlons27 <alonso.morey.arq@gmail.com>
2022-02-01 23:39:30 -0500

    Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
-"""Create class student"""
+""" task 10 """
 
 
 class Student:
-    """Student Class"""
+    """
+    class Student that defines a student by:
+    Public instance attributes:
+    - first_name
+    - last_name
+    - age
+    """
 
     def __init__(self, first_name, last_name, age):
-        """initalitation method"""
-        
+        """Instantiation with first_name, last_name and age"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """that retrieves a dictionary representation of a Student instance"""
-
-        my_dict = dict()
-        if type(attrs) is list and all(type(x) is str for x in attrs):
-            for x in attrs:
-                if x in self.__dict__:
-                    my_dict.update({x: self.__dict__[x]})
-            return my_dict
-        return self.__dict__.copy()
+        """retrieves a dictionary representation of a Student instance"""
+        if attrs is not None:
+            new_dict = {}
+            for i in attrs:
+                if type(i) != str:
+                    return self.__dict__
+            for i in attrs:
+                if i in self.__dict__:
+                    new_dict[i] = self.__dict__[i]
+            return new_dict
+        else:
+            return self.__dict__
