cd1d9432fff24ad937d0af73b0c174d3ffd83a0c
DAlons27 <alonso.morey.arq@gmail.com>
2022-02-13 12:22:38 -0500

    #pythonic

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def magic_string(i=[0]):
-    magic_string.n = getattr(magic_string, 'n', 0) + 1
-    return ("Best, " * (magic_string.n - 1) + "School")
-
+def magic_string(x=[]):
+    x.append("BestSchool")
+    return ", ".join(x)
 
