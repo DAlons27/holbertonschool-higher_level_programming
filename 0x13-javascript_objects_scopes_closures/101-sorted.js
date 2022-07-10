669e502c8af94be62d60793ca6abf1755a2e8035
Diego Morey <85264308+DAlons27@users.noreply.github.com>
2022-07-10 14:37:08 -0500

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,18 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
-const newDic = {};
 
-for (let key in dict) {
-  if (newDic[dict[key]] === undefined) {
-    newDic[dict[key]] = [];
+const newDict = {};
+
+/**
+ * Imports a dictionary of occurrences by user id and computes
+ *  a dictionary of user ids by occurrence.
+ */
+Object.keys(dict).map(function (key) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [];
   }
-  newDic[dict[key]].push(key);
-}
-console.log(newDic);
+  newDict[dict[key]].push(key);
+  return (true);
+});
+
+console.log(newDict);
