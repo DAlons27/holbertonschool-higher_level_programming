4326d9ae4153fe36ec8853cd1e94c3fdeeb10b25
Diego Morey <85264308+DAlons27@users.noreply.github.com>
2022-07-10 14:43:34 -0500

    Create relationship_state.py

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Class:
+    a) State.
+"""
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+
+class State(Base):
+    """
+    Class:
+        Contains the class definition of a State and an instance Base.
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="states")
