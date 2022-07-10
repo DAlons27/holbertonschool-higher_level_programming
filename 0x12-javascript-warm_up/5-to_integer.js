e0bea4acf2e5fdbb4441dd5c108197a5f04b7eee
Diego Morey <85264308+DAlons27@users.noreply.github.com>
2022-07-10 14:30:36 -0500

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
-const num = parseInt(process.argv[2], 10);
-if(isNaN(num)) {
-    console.log('Not a number');
-} else {
-    console.log(`My number: ${num}`);
-}
+const number = parseInt(process.argv[2]);
+console.log(isNaN(number) ? 'Not a number' : 'My number: ' + number);
