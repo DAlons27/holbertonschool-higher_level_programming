1aac6768d3408f479ecc0c341207ea012c91d58f
DAlons27 <alonso.morey.arq@gmail.com>
2024-01-20 17:57:00 -0800

    fix

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-'''
-Write a function that returns True if the object is exactly
-an instance of the specified class; otherwise False.
-'''
+is_same_class = __import__('2-is_same_class').is_same_class
 
-
-def is_same_class(obj, a_class):
-    return issubclass(a_class, type(obj))
+a = 1
+if is_same_class(a, int):
+    print("{} is an instance of the class {}".format(a, int.__name__))
+if is_same_class(a, float):
+    print("{} is an instance of the class {}".format(a, float.__name__))
+if is_same_class(a, object):
+    print("{} is an instance of the class {}".format(a, object.__name__))
